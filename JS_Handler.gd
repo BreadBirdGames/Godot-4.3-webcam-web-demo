625bec87f3c444729100964f807267927ad2356b
cornelius <macrosedk@gmail.com>
2025-06-03 20:25:07 +0200

    Initial version

---
+++ JS_Handler.gd
@@ -0,0 +1,34 @@
+extends Node
+
+var js_update_qr = JavaScriptBridge.create_callback(update_qr) # This reference must be kept
+var js_update_camera = JavaScriptBridge.create_callback(update_camera) # This reference must be kept
+var externalator = JavaScriptBridge.get_interface("externalator")
+var video = externalator.video
+
+const png_prefix = "data:image/png;base64,"
+const jpeg_prefix = "data:image/jpeg;base64,"
+
+var cameraPreview = null
+
+func update_qr(data):
+	# Handle popup, for now just print
+	print(data[0])
+
+func update_camera(data):
+	if cameraPreview != null:
+		var img: Image = Image.new()
+
+		print(data[0])
+		print(png_prefix)
+
+		var p = data[0].right(-png_prefix.length())
+
+		print(p)
+		img.load_png_from_buffer(Marshalls.base64_to_raw(p))
+
+		cameraPreview.texture = ImageTexture.create_from_image(img)
+
+func _ready() -> void:
+	externalator.addGodotFunction('update_qr', js_update_qr)
+	externalator.addGodotFunction('update_camera', js_update_camera)
+	cameraPreview = get_tree().get_nodes_in_group("CameraPreview")[0]
